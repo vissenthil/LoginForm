c41825599be1f14a719efc2ec007943f603898f7
Senthilkumar <vissenthil@gmail.com>
2020-10-29 08:55:45 +0800

    level two

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ require('dotenv').config('SecretsStartingCode\,env');
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
-const encrypt   = require("mongoose-encryption");
+//const encrypt   = require("mongoose-encryption");
+//const md5 = require('md5'); //hashfunction it will change the message or password as hash value and store in db
+const bcrypt = require('bcrypt'); //Level 4 security
+const saltRounds = 10;
 const app = express();
 console.log(process.env.SECRET);
 app.set("view engine", "ejs");
@@ -26,7 +29,7 @@ const userSchema = new mongoose.Schema({
 
 //process.env.API_KEY; //Comes from .env file
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
+//userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields:["password"]});
 // To encrypt Only the passwrod encryptedField["password"]
 
 const User = new mongoose.model("User",userSchema);
@@ -46,19 +49,26 @@ app.get("/register",function(req,res){
 //Post for Register
 app.post("/register",function(req,res){
 
-const newUser = new User ({
-  email: req.body.username,
-  password: req.body.password
+bcrypt.hash(req.body.password,saltRounds, function (err,hash){
+  const newUser = new User ({
+    email: req.body.username,
+    //password: md5(req.body.password) //storing the password using hashfunction
+    password: hash //assining the hash value for the password which is generted using bcrypt.hash above
+  });
+  newUser.save(function(err){
+    if (!err){
+      console.log("User created Successfuly");
+      res.render("secrets");
+    }
+    else{
+      res.send(err);
+    }
+  })
+
 });
-newUser.save(function(err){
-  if (!err){
-    console.log("User created Successfuly");
-    res.render("secrets");
-  }
-  else{
-    res.send(err);
-  }
-})
+
+
+
 
 });
 //End register post here
@@ -67,28 +77,28 @@ newUser.save(function(err){
 //User and password autondication start here
 app.post("/login",function(req,res){
   const userName = req.body.username;
-  const Password = req.body.password;
+  //const Password = md5(req.body.password); //hash value will be generted for the password
+                                           //will be chacked here if inside the dtabase password hash value
+                                          // and now generated hash value are same it will allow
+ const Password = req.body.password;
   console.log(userName + '' + Password);
   User.findOne({email: userName},function(err,foundUser){
     console.log('password:' + foundUser.password);
    if (!err){
      if (foundUser){
-         if (foundUser.password === Password){
-           console.log("User Found");
-             res.render('secrets');
-         }
-     }
-   }
-   else {
-     console.log(err);
-   }
+      bcrypt.compare(Password, foundUser.password, function(err, result) {
+        // Store hash in your password DB.
+        console.log(foundUser.password);
+        if (!err){
+            res.render("secrets");
+        }
+    });
 
-  })
-
-});
+    }
+  }
+})
+})
 //User and password autondication start End here
-
-
 
 
 app.listen(3000,function(err){
